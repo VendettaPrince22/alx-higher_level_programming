7ed73a635578cc8ff8e5b140f7ce09a84fee4321
VendettaPrince22 <pomegah@gmail.com>
2022-04-20 15:31:54 +0000

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,18 +1,13 @@
 #!/usr/bin/node
-const {argv} = require('process');
+const { argv } = require('process');
 let out = [];
 argv.forEach((val, index) => {
-	if (`${index}` == 2)
-	{
-		out = "Argument found";
-	}
-	else if (`${index}` < 2)
-	{
-		out = "No argument";
-	}
-	else
-	{
-		out = "Arguments found";
-	}
+  if (`${index}` === 2) {
+    out = 'Argument found';
+  } else if (`${index}` < 2) {
+    out = 'No argument';
+  } else {
+    out = 'Arguments found';
+  }
 });
 console.log(out);
