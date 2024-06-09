0efdec1f8a021691f5d40e6874c1e3e2c5982d6e
Kairo Prince <pomegah@gmail.com>
2024-06-08 23:01:11 -0400

    To_uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,9 +4,7 @@ def uppercase(str):
     for c in str:
         if ord(c) >= 97 and ord(c) <= 122:
             char = chr(ord(c) - 32)
-            #print("{}".format(char), end="")
         else:
             char = c
-            #print("{}".format(char), end="")
         print("{}".format(char), end="")
     print("")
