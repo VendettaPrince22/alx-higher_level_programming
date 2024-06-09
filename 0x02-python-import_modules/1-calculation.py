e9cd337e06a9b2589d7033b47058659caafba19a
Kairo Prince <pomegah@gmail.com>
2024-06-09 04:42:33 -0400

    Toolbox

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    c = add(a, b)
+    d = sub(a, b)
+    e = mul(a, b)
+    f = div(a, b)
+
+    print("{:d} + {:d} = {:d}".format(a, b, c))
+    print("{:d} - {:d} = {:d}".format(a, b, d))
+    print("{:d} * {:d} = {:d}".format(a, b, e))
+    print("{:d} / {:d} = {:.0f}".format(a, b, f))
