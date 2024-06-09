f6d09652db0315d577cc42c0b1ce550b5b9835d3
Kairo Prince <pomegah@gmail.com>
2024-06-09 08:24:14 -0400

    Replace element

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -14,6 +14,6 @@ def replace_in_list(my_list, idx, element):
     elif idx > len(my_list) + 1:
         return my_list
     else:
-        my_list.pop(idx)
+        del my_list[idx]
         my_list.insert(idx, element)
         return my_list
