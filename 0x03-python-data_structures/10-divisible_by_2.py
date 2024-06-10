7d7b04f1202e904867a73f7349b1df0be1760161
Kairo Prince <pomegah@gmail.com>
2024-06-10 09:06:56 -0400

    divisible by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -8,7 +8,7 @@ def divisible_by_2(my_list=[]):
     Returns list with bool True if is, False if not
     """
     new_list = []
-    for i in range(len(my_list)):
+    for i in my_list:
         if i % 2 == 0:
             new_list.append(True)
         else:
