9ed8e7a276a81d76e3bf599fbbcf87da95bc52bf
Kairo Prince <pomegah@gmail.com>
2024-06-10 22:35:18 -0400

    Replaces all occurrences of  an element in a list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -9,6 +9,6 @@ def search_replace(my_list, search, replace):
         replace: new element
     Returns a new list
     """
-    new_list = list(map(lambda x: 89 if x == search else x, my_list))
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
 
     return new_list
