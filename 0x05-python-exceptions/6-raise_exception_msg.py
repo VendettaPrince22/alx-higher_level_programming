f2a4b22bf6128c9e0ed48f2cfd7b7220cbf76166
Kairo Prince <pomegah@gmail.com>
2024-06-14 13:20:41 -0400

    Raise a message

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def raise_exception_msg(message=""):
+    """Raises a name exception with a message
+    Args:
+        message - the message to be printed as error
+    """
+    raise NameError(message)
