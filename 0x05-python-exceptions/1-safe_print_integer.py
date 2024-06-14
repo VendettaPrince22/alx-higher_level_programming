a24aa7294194cc7050749ee6895c2c960dc02621
Kairo Prince <pomegah@gmail.com>
2024-06-14 01:23:03 -0400

    Prints an integer

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def safe_print_integer(value):
+    """Prints an integer
+    Args:
+        value: number to print
+    """
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError:
+        return False
