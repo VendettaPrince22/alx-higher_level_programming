51a736b34d4656644541a31c09f49d2520bb5712
Kairo Prince <pomegah@gmail.com>
2024-06-13 23:52:29 -0400

    Printing stuff even after an error occurs

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    """Prints x elements of a list
+    Args:
+        my_list - list to work with
+        x - number of elements to print
+    """
+    try:
+        num = 0
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+            num += 1
+        print(" ")
+        return num
+
+    except IndexError:
+        print(" ")
+        return num
