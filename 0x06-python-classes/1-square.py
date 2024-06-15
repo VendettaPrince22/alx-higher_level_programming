f4a8a972a3facbd8d5dda913f5a9bc482d03fafd
Kairo Prince <pomegah@gmail.com>
2024-06-14 23:35:55 -0400

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defining a class Square"""
+
+
+class Square:
+    """Defines a square
+    Attributes:
+        size (int): width and height of the square
+    """
+
+    def __init__(self, size):
+        """Initializes the class Square with size"""
+        self.__size = size
