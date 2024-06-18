38f5b133c99da339f397c601cc4d396b5db3b91e
Kairo Prince <pomegah@gmail.com>
2024-06-18 08:41:49 -0400

    Computing factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,15 +2,16 @@
 const { argv } = require('node:process');
 
 function factorial (x) {
-  let fact = 1;
-
-  while (x > 0) {
-    fact = fact * x;
-    x--;
+  if (x === 0 || x === 1) {
+    return 1;
+  } else {
+    return x * factorial(x - 1);
   }
-
-  return fact;
 }
 
 const num = parseInt(argv[2]);
-console.log(factorial(num));
+if (isNaN(num)) {
+  console.log(1);
+} else {
+  console.log(factorial(num));
+}
