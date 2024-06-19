e08a7037423b31959203552ae3e0a001391a8370
Kairo Prince <pomegah@gmail.com>
2024-06-18 23:24:52 -0400

    Defining a method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Contains the `BaseGeometry` class"""
+
+
+class BaseGeometry:
+    """Defines a BaseGeometry class"""
+
+    def area(self):
+        """Raises an exception"""
+        raise Exception("area() is not implemented")
