c2b3cf12dcb3e4e6f7d333c95593de13a4323cd2
Kairo Prince <pomegah@gmail.com>
2024-06-19 00:40:38 -0400

    Square2

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Contains the `Square` class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Defines a square object
+    Args:
+        size: width and height of square
+    """
+    def __init__(self, size):
+        """Initializes the square with size"""
+        super().integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Returns the area of a square"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Representation of square object"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
