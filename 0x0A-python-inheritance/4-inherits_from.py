cf3b9a0c81c2a82639d546443acd98735de7be01
Kairo Prince <pomegah@gmail.com>
2024-06-18 23:15:25 -0400

    Only subclass of

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Contains the `inherits_from` function"""
+
+
+def inherits_from(obj, a_class):
+    """Checks if object inherits from specific class
+    Args:
+        obj: what to check
+        a_class: class to compare with
+    Returns:
+        bool; True if it is, False otherwise
+    """
+    return issubclass(obj, a_class)
