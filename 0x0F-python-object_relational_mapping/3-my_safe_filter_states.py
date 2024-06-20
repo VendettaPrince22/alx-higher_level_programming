6962985886d150e498e12947362f86b80a2bf2d1
Kairo Prince <pomegah@gmail.com>
2024-06-20 03:32:56 -0400

    Guarding from SQL injections

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+MY_USER = sys.argv[1]
+MY_PASS = sys.argv[2]
+MY_DB = sys.argv[3]
+filter_value = sys.argv[4]
+
+db = MySQLdb.connect(host="localhost",
+                     port=3306,
+                     passwd=MY_PASS,
+                     user=MY_USER,
+                     db=MY_DB)
+cur = db.cursor()
+sql = "SELECT * FROM states WHERE name = %s ORDER BY states.id"
+cur.execute(sql, (filter_value, ))
+myresult = cur.fetchall()
+
+for x in myresult:
+    print(x)
+
+cur.close()
+db.close()
