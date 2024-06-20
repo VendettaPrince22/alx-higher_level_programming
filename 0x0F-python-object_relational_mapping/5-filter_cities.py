628731edfa5ca14206bc83b1de1b8f04a5401d63
Kairo Prince <pomegah@gmail.com>
2024-06-20 05:17:44 -0400

    Takes in an argument and uses WITH clause to list cities

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+MY_USER = sys.argv[1]
+MY_PASS = sys.argv[2]
+MY_DB = sys.argv[3]
+filter_value = sys.argv[4]
+
+db = MySQLdb.connect(host="localhost",
+                     port=3306,
+                     passwd=MY_PASS,
+                     user=MY_USER,
+                     db=MY_DB)
+cur = db.cursor()
+sql = "WITH temporaryTable(id) as \
+    (SELECT id \
+    FROM states \
+    WHERE name = %s) \
+    SELECT name \
+    FROM cities, temporaryTable \
+    WHERE state_id = temporaryTable.id"
+cur.execute(sql, (filter_value, ))
+myresult = cur.fetchall()
+
+j = len(myresult)
+myList = []
+for x in myresult:
+    myList.append(x[0])
+
+y = ", ".join(myList)
+print(y)
+
+cur.close()
+db.close()
