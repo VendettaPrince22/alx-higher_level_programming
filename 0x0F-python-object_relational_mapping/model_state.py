31224996c2de68a40e7f8064244c7784287f0bb0
Kairo Prince <pomegah@gmail.com>
2024-06-20 06:21:06 -0400

    SQLAlchemy start

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Contains the class State"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Class State defining a segment for database SQLAlchemy"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
