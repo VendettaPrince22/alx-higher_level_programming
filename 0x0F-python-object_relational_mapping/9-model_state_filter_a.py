de935df7f8c515c0309e0f892fe8ee3224e796cd
Kairo Prince <pomegah@gmail.com>
2024-06-20 10:49:34 -0400

    Filters entries containing letter a

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+from model_state import Base, State
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+MY_USER = sys.argv[1]
+MY_PASS = sys.argv[2]
+MY_DB = sys.argv[3]
+
+engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                       .format(MY_USER, MY_PASS, MY_DB), pool_pre_ping=True)
+Session = sessionmaker(bind=engine)
+
+session = Session()
+
+for id, name in session.query(State.id, State.name).\
+        filter(State.name.like('%a%')):
+    print("{}: {}".format(id, name))
