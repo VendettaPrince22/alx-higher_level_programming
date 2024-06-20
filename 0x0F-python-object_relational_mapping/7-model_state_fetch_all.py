cdbd0dd14142eecf4a6b1538791c758efe6345ed
Kairo Prince <pomegah@gmail.com>
2024-06-20 10:02:55 -0400

    Querying with sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+MY_USER = sys.argv[1]
+MY_PASS = sys.argv[2]
+MY_DB = sys.argv[3]
+
+engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                       .format(MY_USER, MY_PASS, MY_DB), pool_pre_ping=True)
+Session = sessionmaker(bind=engine)
+
+session = Session()
+
+for id, name in session.query(State.id, State.name):
+    print("{}: {}".format(id, name))
