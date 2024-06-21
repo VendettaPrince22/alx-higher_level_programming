cff09b22888d1c7691e66fd56e1f2c4db3fc839a
Kairo Prince <pomegah@gmail.com>
2024-06-21 00:13:02 -0400

    Working with a second table

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
+"""Lists all `cities` from a database"""
 import MySQLdb
 import sys
 
-MY_USER = sys.argv[1]
-MY_PASS = sys.argv[2]
-MY_DB = sys.argv[3]
+if __name__ == "__main__":
+    MY_USER = sys.argv[1]
+    MY_PASS = sys.argv[2]
+    MY_DB = sys.argv[3]
 
-db = MySQLdb.connect(host="localhost",
-                     port=3306,
-                     passwd=MY_PASS,
-                     user=MY_USER,
-                     db=MY_DB)
-cur = db.cursor()
-sql = "SELECT cities.id, cities.name, states.name \
-    FROM states \
-    INNER JOIN cities ON states.id = cities.state_id \
-    ORDER BY cities.id"
-cur.execute(sql)
-myresult = cur.fetchall()
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         passwd=MY_PASS,
+                         user=MY_USER,
+                         db=MY_DB)
+    cur = db.cursor()
+    sql = "SELECT cities.id, cities.name, states.name \
+        FROM states \
+        INNER JOIN cities ON states.id = cities.state_id \
+        ORDER BY cities.id"
+    cur.execute(sql)
+    myresult = cur.fetchall()
 
-for x in myresult:
-    print(x)
+    for x in myresult:
+        print(x)
 
-cur.close()
-db.close()
+    cur.close()
+    db.close()
