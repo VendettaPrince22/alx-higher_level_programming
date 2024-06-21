9484c8a4cc8b1f9844178f2e706c42060ad3201a
Kairo Prince <pomegah@gmail.com>
2024-06-21 11:38:36 -0400

    New class Square inheriting from rectangle

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Contains the class Square that inherits from Rectangle"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Defines a square object with size
+    Args:
+        size: width and height of the square
+        x: offset x
+        y: offset y
+        id: id number
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initializes the square object"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Overloading representation for square object"""
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width)
