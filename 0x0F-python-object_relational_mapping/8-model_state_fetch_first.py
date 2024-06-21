28d5f8a8112821da84e0865ba0a79f5261323075
Kairo Prince <pomegah@gmail.com>
2024-06-21 00:25:51 -0400

    Fetch an object from a database???

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
+"""Prints the first State object from a database"""
 from model_state import Base, State
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-MY_USER = sys.argv[1]
-MY_PASS = sys.argv[2]
-MY_DB = sys.argv[3]
+if __name__ == "__main__":
+    MY_USER = sys.argv[1]
+    MY_PASS = sys.argv[2]
+    MY_DB = sys.argv[3]
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                       .format(MY_USER, MY_PASS, MY_DB), pool_pre_ping=True)
-Session = sessionmaker(bind=engine)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(MY_USER, MY_PASS, MY_DB),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
 
-session = Session()
+    session = Session()
 
-for id, name in session.query(State.id, State.name).filter(State.id == 1):
-    print("{}: {}".format(id, name))
+    for id, name in session.query(State.id, State.name).filter(State.id == 1):
+        print("{}: {}".format(id, name))
