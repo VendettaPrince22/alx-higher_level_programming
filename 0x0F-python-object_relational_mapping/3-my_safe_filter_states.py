dd8d2b00da1f35e437c531cedbf3d7d22db7a2e3
Kairo Prince <pomegah@gmail.com>
2024-06-21 00:10:02 -0400

    Preventing SQL injections

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
+"""Takes in arguments and displays values matching the argument.
+But with SQL Injection guard"""
 import MySQLdb
 import sys
 
-MY_USER = sys.argv[1]
-MY_PASS = sys.argv[2]
-MY_DB = sys.argv[3]
-filter_value = sys.argv[4]
+if __name__ == "__main__":
+    MY_USER = sys.argv[1]
+    MY_PASS = sys.argv[2]
+    MY_DB = sys.argv[3]
+    filter_value = sys.argv[4]
 
-db = MySQLdb.connect(host="localhost",
-                     port=3306,
-                     passwd=MY_PASS,
-                     user=MY_USER,
-                     db=MY_DB)
-cur = db.cursor()
-sql = "SELECT * FROM states WHERE name = %s ORDER BY states.id"
-cur.execute(sql, (filter_value, ))
-myresult = cur.fetchall()
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         passwd=MY_PASS,
+                         user=MY_USER,
+                         db=MY_DB)
+    cur = db.cursor()
+    sql = "SELECT * FROM states WHERE name = %s ORDER BY states.id"
+    cur.execute(sql, (filter_value, ))
+    myresult = cur.fetchall()
 
-for x in myresult:
-    print(x)
+    for x in myresult:
+        print(x)
 
-cur.close()
-db.close()
+    cur.close()
+    db.close()
