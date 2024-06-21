a6d825db5ff6e0a437b87ec4633a876a0fa9ea5c
Kairo Prince <pomegah@gmail.com>
2024-06-21 00:01:32 -0400

    Filter entries where column matches argument

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
+"""Displays values in a table where name matches argument"""
 import MySQLdb
 import sys
 
-MY_USER = sys.argv[1]
-MY_PASS = sys.argv[2]
-MY_DB = sys.argv[3]
-filter_value = sys.argv[4]
+if __name__ == "__main__":
+    MY_USER = sys.argv[1]
+    MY_PASS = sys.argv[2]
+    MY_DB = sys.argv[3]
+    filter_value = sys.argv[4]
 
-db = MySQLdb.connect(host="localhost",
-                     port=3306,
-                     passwd=MY_PASS,
-                     user=MY_USER,
-                     db=MY_DB)
-cur = db.cursor()
-sql = "SELECT * FROM states WHERE name = '{:s}' ORDER BY states.id"
-cur.execute(sql.format(filter_value))
-myresult = cur.fetchall()
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         passwd=MY_PASS,
+                         user=MY_USER,
+                         db=MY_DB)
+    cur = db.cursor()
+    sql = "SELECT * FROM states WHERE name = '{:s}' ORDER BY states.id"
+    cur.execute(sql.format(filter_value))
+    myresult = cur.fetchall()
 
-for x in myresult:
-    print(x)
+    for x in myresult:
+        print(x)
 
-cur.close()
-db.close()
+    cur.close()
+    db.close()
