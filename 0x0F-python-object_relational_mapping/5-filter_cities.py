6de82270e0ec1ab9b0828bd53bdc4a55d0c14e82
Kairo Prince <pomegah@gmail.com>
2024-06-21 00:16:53 -0400

    Making a connection with 2 tables

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,35 +1,36 @@
 #!/usr/bin/python3
+"""Takes in a `state` and lists `cities` by state.id"""
 import MySQLdb
 import sys
+if __name__ == "__main__":
+    MY_USER = sys.argv[1]
+    MY_PASS = sys.argv[2]
+    MY_DB = sys.argv[3]
+    filter_value = sys.argv[4]
 
-MY_USER = sys.argv[1]
-MY_PASS = sys.argv[2]
-MY_DB = sys.argv[3]
-filter_value = sys.argv[4]
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         passwd=MY_PASS,
+                         user=MY_USER,
+                         db=MY_DB)
+    cur = db.cursor()
+    sql = "WITH temporaryTable(id) as \
+        (SELECT id \
+        FROM states \
+        WHERE name = %s) \
+        SELECT name \
+        FROM cities, temporaryTable \
+        WHERE state_id = temporaryTable.id"
+    cur.execute(sql, (filter_value, ))
+    myresult = cur.fetchall()
 
-db = MySQLdb.connect(host="localhost",
-                     port=3306,
-                     passwd=MY_PASS,
-                     user=MY_USER,
-                     db=MY_DB)
-cur = db.cursor()
-sql = "WITH temporaryTable(id) as \
-    (SELECT id \
-    FROM states \
-    WHERE name = %s) \
-    SELECT name \
-    FROM cities, temporaryTable \
-    WHERE state_id = temporaryTable.id"
-cur.execute(sql, (filter_value, ))
-myresult = cur.fetchall()
+    j = len(myresult)
+    myList = []
+    for x in myresult:
+        myList.append(x[0])
 
-j = len(myresult)
-myList = []
-for x in myresult:
-    myList.append(x[0])
+    y = ", ".join(myList)
+    print(y)
 
-y = ", ".join(myList)
-print(y)
-
-cur.close()
-db.close()
+    cur.close()
+    db.close()
