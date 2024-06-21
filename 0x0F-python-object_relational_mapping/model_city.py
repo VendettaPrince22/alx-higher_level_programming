64f074081ae4505c3b22561bf372089ce58166fa
Kairo Prince <pomegah@gmail.com>
2024-06-21 05:23:49 -0400

    Object Schema definition

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -3,9 +3,10 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base, State
 
+
 class City(Base):
     """Class City defining a segment for database SQLAlchemy"""
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'))
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
