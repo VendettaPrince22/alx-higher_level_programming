f2cf7d3b8dc825bc6fd40762452d1be3287406fb
Kairo Prince <pomegah@gmail.com>
2024-06-20 23:49:44 -0400

    Filters entries containing letter a

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
+"""Lists all `states` with name starting with N from database"""
 import MySQLdb
 import sys
 
-MY_USER = sys.argv[1]
-MY_PASS = sys.argv[2]
-MY_DB = sys.argv[3]
+if __name__ == "__main__":
+    MY_USER = sys.argv[1]
+    MY_PASS = sys.argv[2]
+    MY_DB = sys.argv[3]
 
-db = MySQLdb.connect(host="localhost",
-                     port=3306,
-                     passwd=MY_PASS,
-                     user=MY_USER,
-                     db=MY_DB)
-cur = db.cursor()
-cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id")
-myresult = cur.fetchall()
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         passwd=MY_PASS,
+                         user=MY_USER,
+                         db=MY_DB)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id")
+    myresult = cur.fetchall()
 
-for x in myresult:
-    print(x)
+    for x in myresult:
+        print(x)
 
-cur.close()
-db.close()
+    cur.close()
+    db.close()
