1b2ad5fa12c1993494755b258c1739242bd63a5f
Kairo Prince <pomegah@gmail.com>
2024-06-27 15:16:40 -0400

    Query all State objects from database

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -13,8 +13,9 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(MY_USER, MY_PASS, MY_DB),
                            pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
 
+    Session = sessionmaker(bind=engine)
     session = Session()
 
     for id, name in session.query(State.id, State.name):
