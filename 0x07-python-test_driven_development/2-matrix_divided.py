274014a1620d859fd196414dddeeab956bcabb22
Kairo Prince <pomegah@gmail.com>
2024-07-01 10:16:28 -0400

    Fixing code dividing a matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -10,6 +10,7 @@ def matrix_divided(matrix, div):
     Returns:
         new modified matrix
     """
+    string = "matrix must be a matrix (list of lists) of integers/floats"
     if type(matrix) is not list:
         raise TypeError(
             "matrix must be a matrix (list of lists) of integers/floats")
@@ -19,8 +20,7 @@ def matrix_divided(matrix, div):
                 "matrix must be a matrix (list of lists) of integers/floats")
         for x in k:
             if type(x) is not int and type(x) is not float:
-                raise TypeError("matrix must be a matrix\
-                                (list of lists) of integers/floats")
+                raise TypeError(string)
 
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
