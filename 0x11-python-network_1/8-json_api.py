6e1e4e886213632576af55cad865a0b07d826778
Kairo Prince <pomegah@gmail.com>
2024-07-05 13:13:37 -0400

    Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Takes in a letter and sends a POST to a URL with letter as param"""
+
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    url = 'http://0.0.0.0:5000/search_user'
+    if len(sys.argv) == 3:
+        payload = {'q': sys.argv[2]}
+    else:
+        payload = {'q': ""}
+
+    r = requests.post(url, data=payload)
+    try:
+        my_dict = r.json()
+        if len(my_dict) == 0:
+            print("No result")
+        else:
+            print("[{}] {}".format(my_dict.get('id'), my_dict.get('name')))
+    except requests.exceptions.JSONDecodeError as e:
+        print("Not a valid JSON")
