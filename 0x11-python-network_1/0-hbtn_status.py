c18d873352cc61a2e7ce31cf75b28264e67e77f0
Kairo Prince <pomegah@gmail.com>
2024-07-05 09:12:07 -0400

    Fetching a URL

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Fetches 'https://alx-intranet.hbtn.io/status' """
+
+
+if __name__ == "__main__":
+    import urllib.request
+
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+        print(f'Body response:\n\t- type: {type(html)}')
+        print(f'\t- content: {html}\n\t- utf8 content: {response.msg}')
